03e5ab0056deea260a926f86e2be2746fc5c7caf
Bohr Shaw <bohrshaw@gmail.com>
2012-11-28 16:15:13 +0000

    refactoring make_tcl_ext.sh

--- make_tcl_ext.sh
+++ make_tcl_ext.sh
@@ -5,30 +5,27 @@
 #
 # making an extension in tcl is just one methods of backup options, further more I may consider remastering tcl to include this ext. into os core.
 
-echo "check and delete old files"
-if [ -d ~/exts/vimise ]; then
-    rm -rf ~/exts/vimise
-fi
-
 echo "preparing..."
 tcedir="/mnt/sda6/tce/optional"
-mkdir -p $HOME/exts/vimise/home/tc
 extdir="$HOME/exts/vimise"
 vimisedir="$HOME/exts/vimise/home/tc/vimise"
+# delete old files
+if [ -d $extdir ]; then
+    rm -rf $extdir
+fi
+mkdir -p $HOME/exts/vimise/home/tc
 
 echo "loading squashfs-tools-4.x.tcz"
 tce-load -i $tcedir/squashfs-tools-4.x.tcz
 
 echo "preparing files..."
-cd $HOME
-cp -r vimise $extdir/home/tc
-cp -ax .vim .vimrc .gvimrc .vimperator .vimperatorrc $extdir/home/tc
+cp -r $HOME/vimise $extdir/home/tc
+cp -ax $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimperator $HOME/.vimperatorrc $extdir/home/tc
 rm -rf $vimisedir/vim/tmp
 rm -rf $vimisedir/vimperator/info
 
 echo "making squashfs..."
-cd $HOME/exts
-mksquashfs vimise/ $tcedir/vimise.tcz
+mksquashfs $HOME/exts/vimise/ $tcedir/vimise.tcz -noappend
 
 echo "cleaning up..."
 rm -rf $extdir
