bf9f012110428586a31f6d6c7f429c0f5e09680f
Bohr Shaw <bohrshaw@gmail.com>
2013-01-01 22:50:21 +0800

    initial commit from changing vundle to pathogen
    Change-Id: I76c3b5d14d861f6003a07dbd208dafe338f9376d

--- todo.md
+++ todo.md
@@ -1,17 +1,16 @@
+# What's next?
+Write bundle description in more details in "bundles.md", and adjust the
+"git-clone.sh" script accordingly.
+
+upload a single vimrc to net, then use ":e http://example.com/vimrc"
+to load and source my essential vimrc.
+
+The branch pathogen is used to rebase on the vundle branch from time to time, so
+I should carefully make a commit just for switching form vundle to pathogen
+anytime.
+
 # Project management
 Task management tools wanted.(May consider tools in pure vim environment as long as
 current project is small and the tool has enough power.)
 Need tools for feature request, bug tracking etc.
 I really should learn agile development and _practice_ to be a better developer.
-
-# Change to pathogen to manage plugins
-Vim plugins need to be familiar with before being a part of vim,
-so the installation and removal of plugins
-is not frequent. And for this reason pathogen is preferable than vundle as it's more
-efficient than vundle and also as convenient as it.
-
-# start to refactor vim to " vimrc <= vicrc => vilrc"
-Figure out a way to let vim runtimepath to include the path vilrc located in.
-Thus "vil" will use custom initialization directories(the "vil" folder).
-
-# set vim to auto add a new line when the text inserting reaches window bargain
---
+++ tools/update_vimruntime.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+# This script is used to update vim runtime files(maybe to the develop version).
+# Modified from here
+# "http://wyw.dcweb.cn/download.asp?path=vim&file=update_vimruntime.sh"
+
+# the directory files will be downloaded into
+VIMRUNTIME_DL=`echo ~/vimruntime`
+# the directory to sync with above directory
+VIMRUNTIME_RT="/vagrant/Vim/vim73"
+
+[ -d "$VIMRUNTIME_DL" ] || mkdir -p "$VIMRUNTIME_DL"
+cd "$VIMRUNTIME_DL"
+
+# sync from official repository to local directory, skip based on checksum, keep
+# partially transfered files
+rsync -avzcP --delete-after ftp.nl.vim.org::Vim/runtime/dos/ .
+
+# skip files that are newer on the receiver, and delete extra files from the receiving side
+rsync -avu --delete "autoload/" "$VIMRUNTIME_RT/autoload/"
+rsync -avu --delete "colors/"   "$VIMRUNTIME_RT/colors/"
+rsync -avu --delete "compiler/" "$VIMRUNTIME_RT/compiler/"
+rsync -avu --delete "doc/"      "$VIMRUNTIME_RT/doc/"
+rsync -avu --delete "ftplugin/" "$VIMRUNTIME_RT/ftplugin/"
+rsync -avu --delete "indent/"   "$VIMRUNTIME_RT/indent/"
+rsync -avu --delete "keymap/"   "$VIMRUNTIME_RT/keymap/"
+rsync -avu          "lang/"     "$VIMRUNTIME_RT/lang/"
+rsync -avu --delete "macros/"   "$VIMRUNTIME_RT/macros/"
+rsync -avu --delete "plugin/"   "$VIMRUNTIME_RT/plugin/"
+#rsync -avu --delete "print/"    "$VIMRUNTIME_RT/print/"
+rsync -avu --delete "syntax/"   "$VIMRUNTIME_RT/syntax/"
+rsync -avu --delete "tools/"    "$VIMRUNTIME_RT/tools/"
+rsync -avu --delete "tutor/"    "$VIMRUNTIME_RT/tutor/"
+# preserve modification times
+rsync -tvu *.vim "$VIMRUNTIME_RT/"
