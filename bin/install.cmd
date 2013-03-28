4b4fc01669978b15c16f0a6cb7509c511c881a98
Bohr Shaw <bohrshaw@gmail.com>
2013-03-28 16:11:53 +0800

    Trivial, modify the default repository path from '~/vimise' to '~/configent/vim'
    Change-Id: If3989a771d94185726b5b2cd2a31cd2857079a09

--- bin/install.cmd
+++ bin/install.cmd
@@ -3,23 +3,23 @@ REM Start installation...
 @if not exist "%HOME%" @set HOME=%USERPROFILE%
 
 REM Set path variables.
-@if not exist "%HOME%\vimise" call mklink /J "%HOME%\vimise" "D:\projects\vimise"
-@set VIMISE_DIR=%HOME%\vimise
-@set GIT_DIR=C:\Program Files\Git
+@set VIM_DIR=%HOME%\configent\vim
+@set GIT_DIR=D:\ProgramsPortable\SmartGitHg\git
 @set SYNC_DIR=D:\Sync\SkyDrive\Documents
 
 REM Make links.
-@if not exist "%HOME%\.vim" call mklink /J "%HOME%\.vim" "%VIMISE_DIR%\vim"
-@if not exist "%HOME%\.vimrc" call mklink "%HOME%\.vimrc" "%VIMISE_DIR%\vimrc"
-@if not exist "%HOME%\.gvimrc" call mklink "%HOME%\.gvimrc" "%VIMISE_DIR%\gvimrc"
-@if not exist "%HOME%\vimperator" call mklink /J "%HOME%\vimperator" "%VIMISE_DIR%\vimperator"
-@if not exist "%HOME%\.vimperatorrc" call mklink "%HOME%\.vimperatorrc" "%VIMISE_DIR%\vimperatorrc"
+@if not exist "%HOME%\.vim" call mklink /D "%HOME%\.vim" "%VIM_DIR%\vim"
+@if not exist "%HOME%\.vimrc" call mklink "%HOME%\.vimrc" "%VIM_DIR%\vimrc"
+@if not exist "%HOME%\_vimrc" call mklink "%HOME%\.vimrc" "%VIM_DIR%\vimrc"
+@if not exist "%HOME%\.gvimrc" call mklink "%HOME%\.gvimrc" "%VIM_DIR%\gvimrc"
+@if not exist "%HOME%\vimperator" call mklink /D "%HOME%\vimperator" "%VIM_DIR%\vimperator"
+@if not exist "%HOME%\.vimperatorrc" call mklink "%HOME%\.vimperatorrc" "%VIM_DIR%\vimperatorrc"
 
 REM Make sure bash is ready.
-@if not exist "%GIT_DIR%\cmd\bash.cmd" call copy "%VIMISE_DIR%\bin\bash.cmd" "%GIT_DIR%\cmd\bash.cmd"
+@if not exist "%GIT_DIR%\cmd\bash.cmd" call copy "%VIM_DIR%\bin\bash.cmd" "%GIT_DIR%\cmd\bash.cmd"
 
 REM Sync bundles.
-"%GIT_DIR%\cmd\bash.cmd" %VIMISE_DIR%\bin\git.sh -s
+"%GIT_DIR%\cmd\bash.cmd" %VIM_DIR%\bin\git.sh -s
 
 REM Link the vimwiki repository to the synced directory.
 @if not exist "%HOME%\vimwiki" call mklink /J "%HOME%\vimwiki" "%SYNC_DIR%\VimWiki"
