e0f4e4bfae4d44be1fdcff60f025aadbec43fc47
Bohr Shaw <bohrshaw@gmail.com>
2013-03-31 13:15:10 +0800

    Change the windows specific installation script to a powershell script without other dependences.
    Change-Id: I196cf1669b63d7735495a645908af7368b49f230

---
+++ install.ps1
@@ -0,0 +1,44 @@
+Param ( [Switch] $override=$false )
+
+# Supporting functions#{{{
+function New-Link($target, $link) {
+    # What to do if the link existed.
+    if (Test-Path $link) {
+        if ($script:override) {
+            if (Test-Path -type container $link) {
+                [System.IO.Directory]::Delete($link)
+            }
+            else { remove-item $link }
+        }
+        else { write "$link already existed."; return }
+    }
+
+    # Create the link.
+    if (Test-Path -type container $target) {
+        Invoke-MKLink "/J" $link $target
+    }
+    else { Invoke-MKLink $link $target }
+}
+
+function Invoke-MKLink { cmd /c mklink $args }
+#}}}
+
+# Environment variables
+$vim_dir = Split-Path $script:MyInvocation.MyCommand.Path
+$bundle_dir = $vim_dir + '\vim\bundle'
+$bundle_file = $vim_dir + '\vimrc.bundle'
+$sync_dir = 'D:\Sync\Skydrive'
+
+# Link files
+New-Link "$vim_dir\vim" "$HOME\.vim"
+New-Link "$vim_dir\vimrc.core" "$HOME\.vimrc.core"
+New-Link "$vim_dir\vimrc" "$HOME\.vimrc"
+New-Link "$vim_dir\vimrc" "$HOME\_vimrc"
+New-Link "$vim_dir\vimrc.light" "$HOME\.vimrc.light"
+New-Link "$vim_dir\gvimrc" "$HOME\.gvimrc"
+New-Link "$sync_dir\Documents\VimWiki" "$HOME\vimwiki"
+
+# Sync bundles
+Invoke-Expression "$vim_dir\bin\Sync-Bundle.ps1"
+
+# vim:tw=80 ts=4 sw=4 et fdm=marker:
