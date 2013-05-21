10346067f5a24f9ae1a8495077b5f8e54d11883c
Bohr Shaw <bohrshaw@gmail.com>
2013-05-21 16:36:32 +0800

    Minor fixes
    Change-Id: I1498bbf0314d86fccfb8fac2a04abac6fb123bc3

--- bin/sync-bundle.rb
+++ bin/sync-bundle.rb
@@ -1,45 +1,29 @@
 #!/usr/bin/env ruby
 
-# This tool is not a universal git repository manager. Its main usage is
-# to sync vim bundles. However you may use it as a bulk repository updater.
-# And you can customise this to suit your needs.
-# Based on the 'thor' scripting framework.
-# Documents of 'thor': https://github.com/wycats/thor/wiki
+# Sync vim bundles.
+#
+# Based on the 'thor' - 'https://github.com/wycats/thor/wiki'
 
 require "thor"
 
-# A class for a subcommand.(demo)
-class Subcommand < Thor
-  desc 'sync', "sync bundles"
-  option :update
-  option :delete
-
-  def sync(arg=nil)
-    puts "#{arg}"
-    puts options[:delete]
-  end
-end
-
-# The main class.
-class Git < Thor
+class Bundle < Thor
   # Options for all methods inside this class.
   class_option :verbose, :type => :boolean
 
-  VIM_DIR = File.expand_path('..', File.dirname(__FILE__) )
+  THIS_FILE_DIR = File.expand_path('..', File.dirname(__FILE__) )
+  VIM_DIR = THIS_FILE_DIR.gsub(/\/bin$/, '')
+
   BUNDLES_FILE = "#{VIM_DIR}/vimrc.bundle"
 
-  # Set the working directory.
   Dir.chdir "#{VIM_DIR}/vim/bundle"
 
   # Make an alias to a task.
-  # map 's' => :sync
+  map 's' => :sync
   long_desc <<-LONGDESC
     Enable/Clone, disable/remove bundles.
     \x5> $ git.rb sync [ --update ] [ --delete ]
   LONGDESC
-  # Usage and short description.
   desc 'sync [-u] [-d]', "Sync bundles"
-  # option is an alias for method_option
   option :update, :aliases => '-u', :desc => 'Update the just enabled bundle'
   option :clean, :aliases => '-c', :desc => 'Delete all disabled bundles'
 
@@ -81,40 +65,6 @@ class Git < Thor
     puts 'Syncing bundles done.'
   end
 
-  # map 'u' => :update
-  desc 'update [-a] [-r]', 'Update all repositories under a directory'
-  option :all, :aliases => '-a', :desc => 'Update all repositories include the disabled'
-  option :recursive, :aliases => '-r', :desc => 'Update all repositories recursively'
-
-  def update(root_dir='.')
-    pattern_prepend = root_dir[-1] == '/' ? root_dir.chop : root_dir
-
-    Dir.glob(pattern_prepend + glob_pattern) do |d|
-      d.sub!(/\.git$/, '') 
-      puts "Updating '#{d}'..."
-      Dir.chdir(d) { puts `git pull` }
-    end
-    puts "Update repositories done."
-  end
-
-  # Conceal methods that are not tasks.
-  no_tasks do
-    def glob_pattern
-      if options[:recursive] && options[:all]
-        '/**/*/.git'
-      elsif options[:recursive]
-        '/**/*[^~]/.git' # Exclude the current directory
-      elsif options[:all]
-        '/*/.git'
-      else
-        '/*[^~]/.git'
-      end
-    end
-  end
-
-  desc "subcommand command ...ARGS", "a subcommand"
-  subcommand "subcommand", Subcommand
-
 private
   # Get the full URL based on partial URL like 'partial/smile.git'.
   def get_url(partial_url)
@@ -133,18 +83,18 @@ private
       if Dir.exists? dest + '~'
         puts "Enable #{dest.capitalize}"
         File.rename dest+'~', dest
-
-        if options[:update]
-          puts "Update #{dest.capitalize}"
-          Dir.chdir(dest) { puts `git pull` }
-        end
       else
         puts "Cloning into '#{dest}'..."
         puts `git clone #{url}`
+      end
+    else
+      if options[:update]
+        puts "Update #{dest.capitalize}"
+        Dir.chdir(dest) { puts `git pull` }
       end
     end
   end
 end
 
 # Exclude the bellow line if run 'thor install this_file'
-Git.start
+Bundle.start
