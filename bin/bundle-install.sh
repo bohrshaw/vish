4c6e09b1d8a77adec0a45fc50d21208023a19107
Bohr Shaw <bohrshaw@gmail.com>
2013-06-28 16:11:28 +0800

    Add a script to do extra works to make some bundles properly installed.
    Change-Id: I7e6b692edfca3485877c023203c89d87b3d50a7f

---
+++ bin/bundle-install.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Extra works to installing bundles.
+
+# Pre settings {{{1
+# Get the script path
+pushd `dirname $0` > /dev/null
+script_path=`pwd -P`
+popd > /dev/null
+
+# CD to the bundle directory
+cd $script_path/../bundle
+
+# YouCompleteMe {{{1
+# Ensure you have Vim 7.3.584+ with python2 support
+# Ensure required packages are installed
+pkgs=(build-essential cmake python-dev)
+for p in $pkgs; do
+  dpkg -s $p > /dev/null 2>&1 || sudo apt-get install -y $p
+done
+
+# Start installation(for Linux)
+pushd YouCompleteMe
+# Compiling YCM without semantic support for C-family languages:
+./install.sh
+# Compiling YCM with semantic support for C-family languages:
+# ./install.sh --clang-completer
+popd
+
+# Hammer.vim {{{1
+gem install github-markup tilt redcarpet
+
+# Markdown previewer {{{1
+# Install multimarkdown from source.
+pushd ~/local
+git clone https://github.com/fletcher/MultiMarkdown-4.git multimarkdown
+
+pushd multimarkdown
+git submodule update --init
+touch greg/greg.c # prevent an error which caused the build to fail
+make
+make test-all
+chmod +x multimarkdown
+ln -sf ~/local/multimarkdown/multimarkdown ~/local/bin
+popd
+
+popd
+
+# }}}1
+
+# vim:tw=0 ts=2 sw=2 et fdm=marker:
