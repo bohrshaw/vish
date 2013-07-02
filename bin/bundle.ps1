747ad940058e12a862c934a2bbbd85b9dbd8b281
Bohr Shaw <bohrshaw@gmail.com>
2013-07-02 19:27:08 +0800

    Fix bundle.ps1.
    Create the 'bundle' directory if not existing. Fix the problem when cleaning bundles.
    
    Change-Id: I58c63a467c19f1eeea563d508d25f2d977525997

--- bin/bundle.ps1
+++ bin/bundle.ps1
@@ -1,10 +1,14 @@
 param([switch]$pull = $false, [switch]$clean = $false)
 
 $vim_dir = ( Split-Path $script:MyInvocation.MyCommand.Path ) + '\..'
-$bundle_dir = $vim_dir + '\bundle'
+$bundle_root = $vim_dir + '\bundle'
 $bundle_file = $vim_dir + '\vimrc.bundle'
 
-pushd $bundle_dir
+# Ensure the 'bundle' root directory exists
+if (! (Test-Path $bundle_root)) {
+    mkdir $bundle_root
+}
+pushd $bundle_root
 
 # Clone or pull bundles according to bundles specification.
 function Bundle-Bundle($bundles) {
@@ -17,6 +21,7 @@ function Bundle-Bundle($bundles) {
 
         if (Test-Path $bundle_dir) {
             if ($pull) {
+                echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> $bundle_dir"
                 pushd $bundle_dir; iex "git pull; $gsm"; popd
             }
         }
@@ -25,7 +30,7 @@ function Bundle-Bundle($bundles) {
                 move-item "$bundle_dir~" $bundle_dir
             }
             else {
-                echo $bundle_url
+                echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> $bundle_dir"
                 iex "git clone --depth 1 $bundle_url"
                 pushd $bundle_dir; iex $gsm; popd
             }
@@ -36,12 +41,15 @@ function Bundle-Bundle($bundles) {
 # Clean bundles according to bundles specification.
 function Clean-Bundle($bundles) {
     foreach ($dir in ls) {
+        $dir_is_active = $false
+
         foreach ($bundle in $bundles) {
             $bundle_dir = $bundle.split('/')[-1]
             if ($dir.name -eq $bundle_dir) {
                 $dir_is_active = $true; break
             }
         }
+
         if (! $dir_is_active) {
             remove-item -recurse -force $dir
         }
