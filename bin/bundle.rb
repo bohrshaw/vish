518a87d2221cbb71cb03e116fe0aaf15ab25ded6
Bohr Shaw <bohrshaw@gmail.com>
2013-07-07 16:23:38 +0800

    Refactor and perfect bundle.rb.
    When a bundle repository has many remotes and branches, the updating
    should be carefull and smart.
    
    Change-Id: Ifd0d27e3da44118507a7bd0cae3629ca78c7a12f

--- bin/bundle.rb
+++ bin/bundle.rb
@@ -20,13 +20,16 @@ end
 
 # Parse the command line argument
 ACTION = ARGV.shift
+OPTIONS = ARGV.join.tr('-', '')
 
 # Prompt the usage
-unless [nil, 'pull', 'clean'].include? ACTION
+unless [nil, 'update', 'clean'].include? ACTION
   puts <<-'HERE'
 Usage:
-bundle.rb pull    -- pull all bundles when cloning
-bundle.rb clean   -- clean inactive bundles
+bundle.rb update -- update bundles
+              -u -- update (the default option if omitted)
+              -t -- update tracking infomation
+bundle.rb clean  -- clean obsolete bundles
   HERE
   exit
 end
@@ -40,8 +43,12 @@ def sync_bundles
       bundle_dir = bundle.split('/')[1]
 
       if File.exist? bundle_dir or File.exist? bundle_dir + '~'
-        File.rename bundle_dir + '~', bundle_dir if File.exist? bundle_dir + '~'
-        Dir.chdir(bundle_dir) { update_bundle bundle }
+        if File.exist? bundle_dir + '~'
+          File.rename bundle_dir + '~', bundle_dir
+        end
+        if ACTION == 'update'
+          Dir.chdir(bundle_dir) { update_bundle bundle }
+        end
       else
         clone_bundle bundle
       end
@@ -51,19 +58,60 @@ def sync_bundles
   end
 end
 
-# Pull a bundle
+# Update a bundle
 def update_bundle(bundle)
-  author, bundle_dir = bundle.split('/')
-  author_orig = `git config --get remote.origin.url`.split('/')[-2]
+  if OPTIONS.include?('t')
+    update_tracking bundle
+    update_branch
+    return
+  end
 
-  if author != author_orig
-    FileUtils.rm_rf '.'
-    clone_bundle bundle, '.'
-  elsif ACTION == 'pull'
-    puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Update #{bundle_dir.capitalize}"
-    puts `git pull`
+  if OPTIONS.include?('u') or OPTIONS == ''
+    update_branch
     update_submodules
   end
+end
+
+# Update the branch tracking information
+def update_tracking(bundle)
+  author = bundle.split('/')[0]
+  author_orig = `git ls-remote --get-url`.chomp.split(%r[/|:])[-2]
+
+  # Update only if the current tracking remote URL contains a different author
+  if author.casecmp(author_orig) != 0
+    # Track an existing remote
+    remote_urls = `git config --get-regex 'remote\.[^.]+\.url'`.split(/\r?\n/)
+    remote_urls.each do |url|
+      a_remote = url.split(/\./)[1]
+      a_author = url.split(%r[/|:])[-2]
+
+      if a_author == author
+        track_remote a_remote
+        return
+      end
+    end
+
+    # Track a new remote if the above fails
+    `git remote add #{author} #{get_url(bundle)}`
+    track_remote author
+  end
+end
+
+# Track the default branch of a remote from the current branch
+def track_remote(remote)
+  remote_branch_default = `git name-rev --name-only #{remote}`.chomp
+  `git branch -u #{remote}/#{remote_branch_default}`
+end
+
+# Fetch updates and reset the current branch to the tracking remote branch
+def update_branch
+  # Get the name of the tracking remote of the current local branch
+  branch = `git name-rev --name-only HEAD`.chomp
+  remote = `git config branch.#{branch}.remote`.chomp
+
+  puts ">>>>>>>>>>>>>>>>>>>>>>>>>>> Update #{Dir.pwd.split('/')[-1]}"
+  puts `git fetch #{remote}`
+  `git reset --hard #{remote}`
 end
 
 # Clone a bundle
@@ -79,7 +127,7 @@ end
 def update_submodules
   if File.exist? '.gitmodules'
     `git submodule sync`
-    `git submodule update --init`
+    `git submodule update --init --recursive`
   end
 end
 
