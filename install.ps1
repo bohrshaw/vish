76c1a8c623fe2ae0b8414ebf92181874525d2a0c
Bohr Shaw <bohrshaw@gmail.com>
2013-08-12 19:51:40 +0800

    Update install.ps1.
    Change-Id: Ie58acb49eacd26e065e19fec5002d1e883106f50

--- install.ps1
+++ install.ps1
@@ -1,18 +1,18 @@
-Param ( [Switch] $override=$true )
+Param ( [Switch] $override=$false )
 
-# Supporting functions#{{{
+# Supporting functions {{{
 function New-Link {
-    param([String]$target, [String]$link, [switch]$link_given = $false)
+    param([String]$target, [String]$link)
 
-    # Generate the line path if omitted.
-    if (! $link_given) {
+    # Generate the line path ifomitted.
+    if(! $link) {
         $link = "$HOME\." + $target.split('\')[-1]
     }
 
-    # What to do if the link existed.
-    if (Test-Path $link) {
-        if ($script:override) {
-            if (Test-Path -type container $link) {
+    # What to do ifthe link existed.
+    if(Test-Path $link) {
+        if($script:override) {
+            if(Test-Path -type container $link) {
                 [System.IO.Directory]::Delete($link)
             }
             else { remove-item $link }
@@ -21,28 +21,24 @@ function New-Link {
     }
 
     # Create the link.
-    if (Test-Path -type container $target) {
+    if(Test-Path -type container $target) {
         Invoke-MKLink "/J" $link $target
     }
     else { Invoke-MKLink $link $target }
 }
 
 function Invoke-MKLink { cmd /c mklink $args }
-#}}}
+# }}}
 
 # Environment variables
 $vim_dir = Split-Path $script:MyInvocation.MyCommand.Path
-$sync_dir = 'D:\Sync\Skydrive'
 
 # Link this repository to ~/.vim
-New-Link $vim_dir
+New-Link $vim_dir $HOME\.vim
 
 # Link vimrc files
 $targets = @("vimrc", "vimrc.core", "vimrc.light", "vimrc.bundle", "vsvimrc")
-foreach ( $target in $targets ) { New-Link "$vim_dir\$target" }
-
-# Link vimwiki
-New-Link "$sync_dir\Documents\VimWiki" "$HOME\vimwiki" -link_given
+foreach( $target in $targets ) { New-Link "$vim_dir\$target" }
 
 # Sync bundles
 Invoke-Expression "$vim_dir\bin\bundle.rb"
