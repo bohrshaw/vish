4424c296f1d7fa91a5d65368cd68d70fa31873c0
Bohr Shaw <bohrshaw@gmail.com>
2013-10-15 20:16:25 +0800

    Fix uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/usr/bin/env bash
 # Uninstall the "VIMISE" distribution.
 
 echo "Starting uninstallation..."
@@ -9,7 +9,7 @@ echo "Remove files..."
 rm -rf .vim .vimrc .vimrc.light
 
 echo "Restore backups..."
-for i in .vim.* .vimrc.*; do
+for i in .vim .vimrc*; do
     j=`echo $i | sed -r 's/\.[0-9]+$//g'`
     mv $i $j 2>/dev/null
 done
