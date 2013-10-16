0af402765e9df2e93ff203d91f4f6fb48b347f72
Bohr Shaw <bohrshaw@gmail.com>
2013-10-16 19:57:46 +0800

    Refactor and improve the bundle manager.

--- bin/bundle.rb
+++ bin/bundle.rb
@@ -1,124 +1,133 @@
 #!/usr/bin/env ruby
 
+# Sync Vim bundles
 # Author: Bohr Shaw <pubohr@gmail.com>
-# Description: Sync Vim bundles.
 
 require 'fileutils'
 require 'thwait'
 
-VIM_DIR = File.expand_path('..', File.dirname(__FILE__) )
-BUNDLE_DIR = "#{VIM_DIR}/bundle"
-BUNDLE_FILE = "#{VIM_DIR}/vimrc.bundle"
+# Parse the command line options
+OPTIONS = ARGV.join.gsub(/\W/, '').chars
 
-# Make sure the bundle directory exists
-Dir.mkdir BUNDLE_DIR unless Dir.exist? BUNDLE_DIR
-
-# Set the current working directory
-Dir.chdir BUNDLE_DIR
-
-# Get the bundle list
-BUNDLES = [] # A bundle's format is like "user/repository"
-File.foreach(BUNDLE_FILE) do |line|
-  if line =~ /^\s*Bundle '.*/
-    BUNDLES << line.gsub(/^\s*Bundle ['|"](.*?)['|"].*/, '\1').chomp
-  end
-end
-
-# Parse the command line argument
-ACTION = ARGV.shift
-OPTIONS = ARGV.join.tr('-', '')
-
-# Prompt the usage
-unless [nil, 'update', 'clean'].include? ACTION
+# Print usages
+unless OPTIONS.sort.join =~ /^c?u?$/
   puts <<-'HERE'
-usage: bundle.rb        -- sync bundles
-   or: bundle.rb update -- update bundles
-   or: bundle.rb clean  -- clean bundles
+Vim bundle manager, sync bundles by default.
+
+usage: bundle.rb [options]
+
+options: -u  update bundles
+         -c  clean bundles
   HERE
   exit
 end
 
-# Sync all bundles
-def sync_bundles
-  BUNDLES.each do |bundle|
-    bundle_dir = bundle.split('/')[1]
+# Define constants
+VIM_DIR = File.expand_path('..', File.dirname(__FILE__) )
+BUNDLE_DIR = "#{VIM_DIR}/bundle"
+BUNDLE_FILE = "#{VIM_DIR}/vimrc.bundle"
+GIT_THREADS = []
 
-    # todo: consider the 'rugged' gem the 'parallel' gem.
-    # todo: output the process and the result of syncing bundles (consider popen3)
-    $threads << Thread.new do
-      if File.exist? bundle_dir or File.exist? bundle_dir + '~'
-        File.rename bundle_dir + '~', bundle_dir if File.exist? bundle_dir + '~'
-        update_bundle bundle
-      else
-        clone_bundle bundle
-      end
+# The bundle manager
+class Bundle
+  # Get the bundle list
+  BUNDLES = [] # the format of a bundle is like "author/repo"
+  File.foreach(BUNDLE_FILE) do |line|
+    if line =~ /^\s*Bundle '.*/
+      BUNDLES << line.gsub(/^\s*Bundle ['|"](.*?)['|"].*/, '\1').chomp
     end
   end
 
-  clean_bundles if ACTION == 'clean'
-end
+  class << self
+    # Sync all bundles
+    def sync
+      BUNDLES.each do |bundle|
+        bundle_dir = bundle.split('/')[1]
 
-# Clone a bundle
-def clone_bundle(bundle, dir = nil)
-  dir ||= bundle.split('/')[1]
-  system("git clone --depth 1 --quiet --recursive #{get_url bundle} #{dir}")
-end
+        # todo: consider the 'rugged' gem and the 'parallel' gem.
+        # todo: output the process and the result of syncing bundles (consider popen3)
+        GIT_THREADS << Thread.new do
+          if File.exist? bundle_dir or File.exist? bundle_dir + '~'
+            File.rename bundle_dir + '~', bundle_dir if File.exist? bundle_dir + '~'
+            update bundle
+          else
+            clone bundle
+          end
+        end
+      end
 
-# Update a bundle
-def update_bundle(bundle)
-  author, repo = bundle.split('/')
-  author_current = `cd #{repo} && git ls-remote --get-url`.chomp.split(%r[/|:])[-2]
+      clean if OPTIONS.include? 'c'
+    end
 
-  if author.casecmp(author_current) != 0
-    FileUtils.rm_rf repo
-    clone_bundle bundle
-  elsif ACTION == 'update'
-    update_branch repo
+    # Clone a bundle
+    def clone(bundle, dir = bundle.split('/')[1])
+      system("git clone --depth 1 --quiet --recursive #{get_url bundle} #{dir}")
+    end
+
+    # Update a bundle
+    def update(bundle)
+      author, repo = bundle.split('/')
+      author_current = `cd #{repo} && git ls-remote --get-url`.chomp.split(%r[/|:])[-2]
+
+      if author.casecmp(author_current) != 0
+        FileUtils.rm_rf repo
+        clone bundle
+      elsif OPTIONS.include? 'u'
+        update_branch repo
+      end
+    end
+
+    # Clean obsolete bundles.
+    def clean
+      bundle_dirs = BUNDLES.map do |b|
+        b.split('/')[-1]
+      end
+
+      Dir.glob('*/').each do |b|
+        FileUtils.rm_rf(b) unless bundle_dirs.include? b.chomp('/')
+      end
+    end
+
+    # Fetch updates and reset the current branch to the tracking remote branch
+    def update_branch(repo)
+      # Get the tracking remote name of the current local branch
+      # branch_name = `cd #{repo} && git name-rev --name-only HEAD`.chomp
+      # remote_name = `cd #{repo} && git config branch.#{branch_name}.remote`.chomp
+      `cd #{repo} && git fetch && git reset --hard origin`
+
+      # Update submodules
+      if File.exist? "#{repo}/.gitmodules"
+        `cd #{repo} && git submodule sync && git submodule update --init --recursive`
+      end
+    end
+
+    # Get the full git URL based on a partial URL like 'author/repo'.
+    def get_url(partial_url)
+      case partial_url
+      when  %r{^(\w+://|(.*?@)?[^.]+\.[^.]+:)}
+        partial_url
+      when %r{^[^/]+/[^/]+$}
+        'git://github.com/' + partial_url + '.git'
+      else
+        abort "This partial git URL '#{partial_url}' is not recognised."
+      end
+    end
   end
 end
 
-# Clean obsolete bundles.
-def clean_bundles
-  bundle_dirs = BUNDLES.map do |b|
-    b.split('/')[-1]
-  end
+# Make sure the bundle directory exists
+Dir.mkdir BUNDLE_DIR unless Dir.exist? BUNDLE_DIR
 
-  Dir.glob('*/').each do |b|
-    FileUtils.rm_rf(b) unless bundle_dirs.include? b.chomp('/')
-  end
-end
+# Change the current working directory
+Dir.chdir BUNDLE_DIR
 
-# Fetch updates and reset the current branch to the tracking remote branch
-def update_branch(repo)
-  # Get the tracking remote name of the current local branch
-  # branch_name = `cd #{repo} && git name-rev --name-only HEAD`.chomp
-  # remote_name = `cd #{repo} && git config branch.#{branch_name}.remote`.chomp
-  `cd #{repo} && git fetch && git reset --hard origin`
-
-  # Update submodules
-  if File.exist? "#{repo}/.gitmodules"
-    `cd #{repo} && git submodule sync && git submodule update --init --recursive`
-  end
-end
-
-# Get the full URL based on a partial URL like 'partial/smile.git'.
-def get_url(partial_url)
-  case partial_url
-  when  %r{^(https?|git|ssh)://.*?/.*?/.*$}
-    partial_url
-  when %r{^[^/]+/[^/]+$}
-    'git://github.com/' + partial_url + '.git'
-  end
-end
-
-# Start execution
-$threads = []
-sync_bundles
+# Start syncing bundles
+Bundle.sync
 
 # Ensure all git operations have finished
-ThreadsWait.all_waits(*$threads)
+ThreadsWait.all_waits(*GIT_THREADS)
 
-# Generate help tags
+# Generate Vim help tags
 cmd = %w{vim -Nesu NONE --cmd}
 cmd += ['if &rtp !~# "\v[\/]\.vim[,|$]" | set rtp^=~/.vim | endif |
         call pathway#setout() | Helptags | qa']
