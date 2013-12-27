33f326c11a0cce8b6baa2584e257d180b172d4be
Bohr Shaw <bohrshaw@gmail.com>
2013-12-27 15:05:59 +0800

    Update the Vim building script

--- bin/vim-builder.sh
+++ bin/vim-builder.sh
@@ -1,34 +1,35 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 # This script compiles and installs vim under ubuntu. Reference links:
 # https://github.com/Valloric/YouCompleteMe/wiki/Building-Vim-from-source
 # http://vim.wikia.com/wiki/Building_Vim
 #
 # Author: Bohr Shaw <pubohr@gmail.com>
-# License: Distributes under the same terms as vim
+# License: Distributes under the same terms as Vim
 
-# Pre-building {{{1
+# Preparation {{{1
 # Restore the default PATH
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
 
-# Ensure all the pre-required packages are installed
-# Use `apt-get build-dep vim-gnome` to check what packages needed.
-# Choose the essential.
-pkgs=(libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python python-dev python3 python3-dev ruby-dev lua5.2 liblua5.2-dev mercurial checkinstall)
+# Determine if X server(GUI) is running
+pidof 'X' &>/dev/null && X_SERVER=1
 
-for p in $pkgs; do
-  dpkg -s $p > /dev/null 2>&1 || sudo apt-get install -y $p
-done
+# Use `apt-get build-dep vim-gnome` to check which packages are needed
+sudo apt-get install -y python python-dev python3 python3-dev ruby1.9.1 \
+  ruby1.9.1-dev lua5.2 liblua5.2-dev mercurial
+if [ $X_SERVER ]; then
+  sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
+    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev \
+    libxpm-dev libxt-dev
+fi
 
-# Purge system vim if installed. (Not necessary if you plan to install to /usr/local/.)
-# sudo apt-get remove vim-gnome vim-nox vim-runtime vim-tiny vim-common vim-gui-common --auto-remove
+# Purge system Vim (unnecessary if install to /usr/local/)
+# sudo apt-get remove vim-gnome vim-nox vim-runtime vim-tiny vim-common \
+#   vim-gui-common --auto-remove
 
-# Prepare the source of vim {{{1
-vim_src='vim-src'
-
-# Get the source
+# Get the source code of Vim
+vim_src='vim'
 if [[ $(git config --get-regex remote.*url) == *b4winckler/vim* ]]; then
-  # Clean and update the repository
   make distclean
   git reset --hard; git clean -dxfq
   git pull
@@ -41,39 +42,41 @@ else
   cd $vim_src
 fi
 
-# Configure, compile and install vim {{{1
+# Configure, compile and install Vim {{{1
 pushd src
 
-./configure \
+conf_cmd="./configure \
+  --with-features=huge \
+  --enable-pythoninterp \
+  --enable-python3interp \
+  --enable-rubyinterp \
+  --enable-luainterp \
+  --enable-multibyte \
+  --disable-netbeans \
   --enable-fail-if-missing \
   --prefix=/usr/local \
-  --with-features=huge \
-  --enable-gui=gnome2 \
-  --enable-pythoninterp=yes \
-  --enable-python3interp=yes \
-  --enable-rubyinterp=yes \
-  --enable-luainterp=yes \
-  --enable-multibyte \
-  --enable-cscope \
-  --disable-netbeans \
-  --with-compiledby="Bohr Shaw <pubohr@gmail.com>" \
-  --quiet # do not print 'checking...' messages
+  --with-compiledby='Bohr Shaw' \
+  --quiet"
   # --with-python-config-dir= \
   # --with-python3-config-dir= \
   # --enable-perlinterp \
+  # --enable-cscope \
   # --with-global-runtime=$HOME/.vim,$VIM/vimfiles,$VIMRUNTIME,$VIM/vimfiles/after,$HOME/.vim/after \
+
+if [ $X_SERVER ]; then
+  "$conf_cmd --enable-gui=gnome2"
+else
+  $conf_cmd
+fi
 
 make
 
-# Install vim
+# Install Vim (prepackaged in '.deb' with 'sudo checkinstall')
 sudo make install
-
-# Install vim prepackaged with a debian package format
-# sudo checkinstall
 
 popd
 
-# Set vim as your default editor with update-alternatives {{{1
+# Set Vim as your default editor
 sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 1
 sudo update-alternatives --set editor /usr/local/bin/vim
 sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 1
