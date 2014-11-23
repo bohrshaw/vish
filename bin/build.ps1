16e390c3f42e6db660c4fd28624fb0ceec360935
Bohr Shaw <bohrshaw@gmail.com>
2014-11-23 16:15:16 +0800

    Update the script for building Vim

--- bin/build.ps1
+++ bin/build.ps1
@@ -1,12 +1,8 @@
-# Build Vim(32-bit) for windows.
+# Build Vim(32-bit) on windows.
 #
 # Reference:
 # src/INSTALLpc.txt, src/Make_mvc.mak
-#
-# Requirements:
-# Visual Studio, Git or Mercurial, 7-zip, Python27, Python33, Ruby, Luajit
-
-# You should compile ruby from source as the binary from "rubyinstaller.org" is built on mingw.
+# http://tuxproject.de/projects/vim/_compile.bat.php
 #
 # Author: Bohr Shaw (mailto:pubohr@gmail.com)
 # License: Distributes under the same terms as vim
@@ -24,25 +20,25 @@ foreach {
 }
 popd
 
-$vim = "vim" # the directory name of Vim's source
+# Paths of langauges to be compiled into Vim
 $python = "C:\Python27"
-$python3 = "C:\Python33"
-$ruby = "D:\Programs\Ruby20"
+$python3 = "C:\Python34"
+$ruby = "D:\Programs\Ruby21mswin" # avoid "rubyinstaller.org" and compile ruby with vs
 $lua = "D:\Workspaces\builds\luajit\src"
 
-# Source Code {{{1
+# Source Code Preparation {{{1
 if($script:Proxy) {
   $http_proxy = "http://localhost:8087"
   $https_proxy = $http_proxy
 }
 
 if((git config --get-regex remote.*url) -match '.*b4winckler/vim.*') {
-  nmake clean
+  nmake -f Make_mvc.mak clean
   git reset --hard; git clean -dxfq
   git pull
 }
 elseif((hg paths default) -match '.*vim.*') {
-  nmake clean
+  nmake -f Make_mvc.mak clean
   hg pull --config http_proxy.host=$http_proxy
   hg update -C; hg purge --all
 }
@@ -52,6 +48,7 @@ else {
   else
     $git_protocol = 'git'
   }
+  $vim = "vim" # the directory to clone Vim source file to
   git clone --depth 1 $git_protocol"://github.com/b4winckler/vim.git" $vim
   cd $vim
 }
@@ -65,8 +62,8 @@ nmake -f Make_mvc.mak `
   CPUNR=i686 WINVER=0x0500 `
   FEATURES=HUGE GUI=yes OLE=no MBYTE=yes IME=yes `
   PYTHON=$python DYNAMIC_PYTHON=yes PYTHON_VER=27 `
-  PYTHON3=$python3 DYNAMIC_PYTHON3=yes PYTHON3_VER=33 `
-  RUBY=$ruby DYNAMIC_RUBY=yes RUBY_VER=20 RUBY_VER_LONG=2.0.0 RUBY_PLATFORM=i386-mswin32_120 RUBY_INSTALL_NAME=msvcr120-ruby200 `
+  PYTHON3=$python3 DYNAMIC_PYTHON3=yes PYTHON3_VER=34 `
+  RUBY=$ruby DYNAMIC_RUBY=yes RUBY_VER=21 RUBY_VER_LONG=2.1.0 RUBY_PLATFORM=i386-mswin32_120 RUBY_INSTALL_NAME=msvcr120-ruby210 `
   LUA=$lua DYNAMIC_LUA=yes LUA_VER=51 `
   USERNAME=bohrshaw USERDOMAIN=gmail.com
 
@@ -75,20 +72,23 @@ nmake -f Make_mvc.mak `
   SDK_INCLUDE_DIR="C:\Program Files\Microsoft SDKs\Windows\v7.1A\Include" `
   CPUNR=i686 WINVER=0x0500 `
   FEATURES=BIG MBYTE=yes `
-  PYTHON3=$python3 DYNAMIC_PYTHON3=yes PYTHON3_VER=33 `
+  PYTHON3=$python3 DYNAMIC_PYTHON3=yes PYTHON3_VER=34 `
   USERNAME=bohrshaw USERDOMAIN=gmail.com
 
 popd
 
-# Packaging {{{1
+# Installation {{{1
 cp src\*.exe,src\*.dll,src\xxd\xxd.exe,vimtutor.bat,README.txt,$lua\*.dll runtime
 mkdir runtime\GvimExt 2>$null
 (ls .\src\GvimExt) -match '.*\.(dll|bat|inf|reg|txt)$' | cp -Destination .\runtime\GvimExt
 mkdir runtime\VisVim 2>$null
 (ls .\src\VisVim) -match '.*\.(dll|bat|inf|reg|txt)$' | cp -Destination .\runtime\VisVim
 
-# mkdir Vim; mv runtime Vim\vim74
-# & 7z a -mx=9 vim-bohr.7z Vim
-# mv Vim\vim74 runtime; rmdir Vim
+$dir = "D:/Programs/Vim/vim74"
+mkdir $dir -ErrorAction Ignore
+rm $dir".bak" -ErrorAction Ignore
+mv $dir $dir".bak"
+cp -r runtime $dir
+invoke-expression $dir"/gvim.exe"
 
 # vim:tw=0 ts=2 sw=2 et fdm=marker:
