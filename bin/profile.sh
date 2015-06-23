88f9e7ae481764b6b49a981a3c3d4dbfff65ded8
Bohr Shaw <bohrshaw@gmail.com>
2015-06-23 18:23:01 +0800

    Improve profile.sh

--- bin/profile.sh
+++ bin/profile.sh
@@ -1,3 +1,7 @@
-# Invoke like `profile.sh --cmd \'let h=1\'
-eval "vim --startuptime startup.log --cmd 'profile start profile.log |profile file *' -c 'profdel file * |qa!' $*"
-vim profile.log startup.log -c 'call profile#tabular() |w'
+#!/usr/bin/env bash
+
+# Invoke like `v=vim l=1 profile.sh`
+[[ -z "$v" ]] && v=nvim
+[[ -z "$l" ]] && l=0
+$v --startuptime startup.log --cmd "let l=$l|profile start profile.log|profile file *" -c 'profdel file *|qa!' "$@"
+$v profile.log startup.log -c 'call profile#tabular()|w|bn|$'
