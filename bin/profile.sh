f96367771cb7feca490478086885c48412d75543
Bohr Shaw <bohrshaw@gmail.com>
2015-06-24 09:44:05 +0800

    Give a more detailed example for profiling

--- bin/profile.sh
+++ bin/profile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Invoke like `v=vim l=1 profile.sh`
+# Invoke like `v=vim l=1 profile.sh --cmd 'let h=1' t.rb`
 [[ -z "$v" ]] && v=nvim
 [[ -z "$l" ]] && l=0
 $v --startuptime startup.log --cmd "let l=$l|profile start profile.log|profile file *" -c 'profdel file *|qa!' "$@"
