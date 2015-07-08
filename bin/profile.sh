e00765c89c91d3a751c01352be5360ac022f36f5
Bohr Shaw <bohrshaw@gmail.com>
2015-07-08 13:39:47 +0800

    Add notes

--- bin/profile.sh
+++ bin/profile.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+#
+# Check Vim start-up performance
+# Related options: 'verbose' and 'verbosefile', or -V
 
 # Invoke like `v=vim l=1 profile.sh --cmd 'let h=1' t.rb`
 [[ -z "$v" ]] && v=nvim
 [[ -z "$l" ]] && l=0
-$v --startuptime startup.log --cmd "let l=$l|profile start profile.log|profile file *" -c 'profdel file *|qa!' "$@"
-$v profile.log startup.log -c 'call profile#tabular()|w|bn|$'
+$v --startuptime startup.log \
+  --cmd "let l = $l | profile start profile.log | profile file *" \
+  -c 'profdel file * | qall!' "$@"
+$v profile.log startup.log \
+  -c 'call profile#tabular() | write | bnext | $'
