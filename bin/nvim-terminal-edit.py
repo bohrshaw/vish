49ca1e25c73be4aa96550244390ce851ea8b6da4
Bohr Shaw <bohrshaw@gmail.com>
2016-04-11 10:45:20 +0800

    Update nvim-terminal-edit.py

--- bin/nvim-terminal-edit.py
+++ bin/nvim-terminal-edit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 # https://github.com/mhinz/neovim-remote/blob/master/bin/nvr
 # https://gist.github.com/tarruda/37f7a3e22996addf8921
@@ -7,39 +7,18 @@
 """Edit a file in the host nvim instance."""
 import os
 import sys
-
 from neovim import attach
 
 args = sys.argv[1:]
 if not args:
-    print "Usage: {} <filename> ...".format(sys.argv[0])
+    print("Usage: {} <filename> ...".format(sys.argv[0]))
     sys.exit(1)
 
 addr = os.environ.get("NVIM_LISTEN_ADDRESS", None)
 if not addr:
     os.execvp('nvim', args)
-
 nvim = attach("socket", path=addr)
 
-
-def _setup():
-    nvim.input('<c-\\><c-n>')  # exit terminal mode
-    chid = nvim.channel_id
-    nvim.command('augroup EDIT')
-    nvim.command('au BufEnter <buffer> call rpcnotify({0}, "n")'.format(chid))
-    # nvim.command('au BufEnter <buffer> startinsert')
-    nvim.command('augroup END')
-    nvim.vars['files_to_edit'] = [os.path.abspath(x) for x in args]
-    for x in args:
-        nvim.command('exe "drop ".remove(g:files_to_edit, 0)')
-
-
-def _exit(*args):
-    nvim.vars['files_to_edit'] = None
-    nvim.command('augroup EDIT')
-    nvim.command('au!')
-    nvim.command('augroup END')
-    nvim.session.stop()
-
-
-nvim.session.run(_exit, _exit, _setup)
+nvim.input('<c-\\><c-n>')  # exit terminal mode
+nvim.command('exe "drop {}"'.format(' '.join(
+    [os.path.abspath(f).replace(' ', '\\\\ ') for f in args])))
